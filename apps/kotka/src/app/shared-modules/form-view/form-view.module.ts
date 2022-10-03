f4ced0dae14a94ae4b313129f3938d6c00710c5a
aorin <meeri.rannisto@gmail.com>
2022-10-03 13:50:55 +0300

    add a loading spinner

--- apps/kotka/src/app/shared-modules/form-view/form-view.module.ts
+++ apps/kotka/src/app/shared-modules/form-view/form-view.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormViewComponent } from './form-view/form-view.component';
 import { KotkaUiMainContentModule } from '../../../../../../libs/kotka/ui/main-content/src';
 import { KotkaUiLajiFormModule } from '../../../../../../libs/kotka/ui/laji-form/src';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [FormViewComponent],
   imports: [
     CommonModule,
+    SharedModule,
     KotkaUiMainContentModule,
     KotkaUiLajiFormModule
   ],
