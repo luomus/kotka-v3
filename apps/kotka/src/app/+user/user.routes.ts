a0f9c696150f5a12233a758afd814d2b19d13c18
aorin <meeri.rannisto@gmail.com>
2022-12-21 13:28:13 +0200

    user route shows not found component

--- apps/kotka/src/app/+user/user.routes.ts
+++ apps/kotka/src/app/+user/user.routes.ts
@@ -3,6 +3,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { OnlyLoggedIn } from '../shared/services/only-logged-in';
+import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
 
 export const userRoutes: Routes = [
   {
@@ -15,7 +16,8 @@ export const userRoutes: Routes = [
     pathMatch: 'full',
     component: LogoutComponent,
     canActivate: [OnlyLoggedIn]
-  }
+  },
+  { path: '', pathMatch: 'full', component: NotFoundComponent }
 ];
 
 export const routing: ModuleWithProviders<RouterModule> = RouterModule.forChild(userRoutes);
