b48b75e15bfc7f9a8891164ffa319b10f8d8b03f
aorin <meeri.rannisto@gmail.com>
2023-01-20 10:51:57 +0200

    use different env file for e2e tests

--- tools/scripts/run-kotka-e2e.sh
+++ tools/scripts/run-kotka-e2e.sh
@@ -4,9 +4,9 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPT_PATH}/../../
 
-docker-compose -f ./docker-compose.e2e.yml down --remove-orphans -v
-docker-compose -f ./docker-compose.e2e.yml up --build kotka-e2e
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e down --remove-orphans -v
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e up --build kotka-e2e
 
 # Give docker some time to finish after test run
 sleep 5
-docker-compose -f ./docker-compose.e2e.yml down --remove-orphans -v
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e down --remove-orphans -v
