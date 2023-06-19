e58c8355a8c14c080539e1415d57031a19b30d78
aorin <meeri.rannisto@gmail.com>
2023-06-19 12:03:23 +0300

    permits info works with the new transaction form schema where the visibility of the permit section can change

--- apps/kotka/src/app/+transactions/transaction-form/transaction-form.component.ts
+++ apps/kotka/src/app/+transactions/transaction-form/transaction-form.component.ts
@@ -10,11 +10,12 @@ import {
 } from '@angular/core';
 import { DataType } from '../../shared/services/data.service';
 import { LajiForm, Person, SpecimenTransaction } from '@kotka/shared/models';
-import { Observable, of, ReplaySubject, Subscription, switchMap } from 'rxjs';
-import { distinctUntilChanged, map, tap } from 'rxjs/operators';
+import { Observable, of, shareReplay, Subscription, switchMap } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { FormService } from '../../shared/services/form.service';
 import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { FormViewComponent } from '../../shared-modules/form-view/form-view/form-view.component';
 
 interface CbdResult {
   grouped?: {
@@ -45,26 +46,22 @@ interface LinkData {
 export class TransactionFormComponent implements OnInit, OnDestroy {
   dataType = DataType.transaction;
 
-  @ViewChild('permitsInfo') permitsInfoTpl!: TemplateRef<any>;
-
-  private geneticResourceAcquisitionCountrySubject = new ReplaySubject<string|undefined>();
-  private geneticResourceAcquisitionCountry$: Observable<string|undefined>;
+  @ViewChild(FormViewComponent, { static: true }) formView!: FormViewComponent;
+  @ViewChild('permitsInfo', { static: true }) permitsInfoTpl!: TemplateRef<any>;
 
   private permitsInfoSub?: Subscription;
 
   private cbdUrl = 'https://api.cbd.int/api/v2013/index/select?fl=id,+identifier_s,+uniqueIdentifier_s,+url_ss,+government_s,rec_countryName:government_EN_t,+rec_title:title_EN_t,+rec_summary:description_t,rec_type:type_EN_t,+entryIntoForce_dt,adoption_dt,retired_dt,limitedApplication_dt&group=true&group.field=governmentSchemaIdentifier_s&group.limit=10&group.ngroups=true&q=(realm_ss:abs)+AND+NOT+version_s:*+AND+schema_s:(authority+absProcedure+absNationalReport)+AND+government_s:%country%&rows=500&start=0&wt=json';
   private maxCountryLinks = 3;
+  private countryLinksCache: Record<string, Observable<LinkData[]>> = {};
+  private prevCountry?: string;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private formService: FormService,
     private renderer: Renderer2,
     private httpClient: HttpClient
-  ) {
-    this.geneticResourceAcquisitionCountry$ = this.geneticResourceAcquisitionCountrySubject.pipe(
-      distinctUntilChanged()
-    );
-  }
+  ) {}
 
   ngOnInit() {
     this.permitsInfoSub = this.getPermitsInfoSub();
@@ -89,20 +86,19 @@ export class TransactionFormComponent implements OnInit, OnDestroy {
     return formData;
   }
 
-  onFormDataChange(formData: Partial<SpecimenTransaction>) {
-    this.geneticResourceAcquisitionCountrySubject.next(formData.geneticResourceAcquisitionCountry);
-  }
-
   private getPermitsInfoSub(): Subscription {
-    return this.geneticResourceAcquisitionCountry$.pipe(
-      tap(() => this.updatePermitsInfo()),
+    return this.formView.formDataChange.pipe(
+      map((data: Partial<SpecimenTransaction>) => data.geneticResourceAcquisitionCountry),
+      tap((country) => {
+        if (country && !this.countryLinksCache[country]) {
+          this.updatePermitsInfo(); // show the info without the country while the country links are loading
+        }
+      }),
       switchMap(country => this.getCountryLinks(country).pipe(
         map(countryLinks => ({ country, countryLinks }))
       ))
     ).subscribe(({ country, countryLinks }) => {
-      if (country) {
-        this.updatePermitsInfo(country, countryLinks);
-      }
+      this.updatePermitsInfo(country, countryLinks);
     });
   }
 
@@ -111,33 +107,39 @@ export class TransactionFormComponent implements OnInit, OnDestroy {
       return of([]);
     }
 
-    const url = this.cbdUrl.replace('%country%', country.toLocaleLowerCase());
-    return this.httpClient.get<CbdResult>(url).pipe(map(result => {
-      const countryLinks: LinkData[] = [];
+    if (!this.countryLinksCache[country]) {
+      const url = this.cbdUrl.replace('%country%', country.toLocaleLowerCase());
+      this.countryLinksCache[country] = this.httpClient.get<CbdResult>(url).pipe(
+        map(result => {
+          const countryLinks: LinkData[] = [];
 
-      (result.grouped?.governmentSchemaIdentifier_s?.groups || []).forEach(data => {
-        (data.doclist?.docs || []).forEach(doc => {
-          if (countryLinks.length >= this.maxCountryLinks) {
-            return;
-          }
-          if (doc.rec_title && doc.url_ss?.[0]) {
-            countryLinks.push({ url: doc.url_ss[0], text: doc.rec_title });
-          }
-        });
-      });
+          (result.grouped?.governmentSchemaIdentifier_s?.groups || []).forEach(data => {
+            (data.doclist?.docs || []).forEach(doc => {
+              if (countryLinks.length >= this.maxCountryLinks) {
+                return;
+              }
+              if (doc.rec_title && doc.url_ss?.[0]) {
+                countryLinks.push({url: doc.url_ss[0], text: doc.rec_title});
+              }
+            });
+          });
 
-      return countryLinks;
-    }));
+          return countryLinks;
+        }),
+        shareReplay(1)
+      );
+    }
+
+    return this.countryLinksCache[country];
   }
 
   private updatePermitsInfo(country?: string, countryLinks?: LinkData[]) {
-    const oldElem: HTMLElement|null|undefined = this.document.getElementById("permitsInfo");
-    const parentElem: HTMLElement|null|undefined = oldElem?.parentElement ||
-      this.document.getElementsByClassName("nagoya-fields")?.[0]?.parentElement?.parentElement;
+    const { oldElem, parentElem } = this.getPermitsInfoElems();
 
-    if (!parentElem) {
+    if (!parentElem || (oldElem && this.prevCountry === country)) {
       return;
     }
+    this.prevCountry = country;
 
     const view = this.permitsInfoTpl.createEmbeddedView({ country, countryLinks });
     view.detectChanges();
@@ -148,5 +150,13 @@ export class TransactionFormComponent implements OnInit, OnDestroy {
       this.renderer.removeChild(parentElem, oldElem);
     }
     this.renderer.appendChild(parentElem, newElem);
+  }
+
+  private getPermitsInfoElems() {
+    const oldElem: HTMLElement|null|undefined = this.document.getElementById("permitsInfo");
+    const parentElem: HTMLElement|null|undefined = oldElem?.parentElement ||
+      this.document.getElementsByClassName("nagoya-fields")?.[0]?.parentElement?.parentElement;
+
+    return { oldElem, parentElem };
   }
 }
