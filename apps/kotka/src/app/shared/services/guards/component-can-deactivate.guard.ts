54b3992fb1ace2b784f035687fa44776aecc4e7e
aorin <meeri.rannisto@gmail.com>
2023-10-24 10:40:43 +0300

    fix lint issues

--- apps/kotka/src/app/shared/services/guards/component-can-deactivate.guard.ts
+++ apps/kotka/src/app/shared/services/guards/component-can-deactivate.guard.ts
@@ -7,4 +7,4 @@ export interface ComponentCanDeactivate {
 
 export const ComponentCanDeactivateGuard: CanDeactivateFn<ComponentCanDeactivate> = (component: ComponentCanDeactivate) => (
     component.canDeactivate()
-)
+);
