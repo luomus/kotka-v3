1d898883277dfa516f25d0c70a30d439c3d747c2
aorin <meeri.rannisto@gmail.com>
2024-04-17 13:51:08 +0300

    fix a sentence having a wrong letter case

--- libs/kotka/pipes/src/lib/pipes.module.ts
+++ libs/kotka/pipes/src/lib/pipes.module.ts
@@ -3,6 +3,7 @@ import { LabelPipe } from './label.pipe';
 import { ReversePipe } from './reverse.pipe';
 import { ToFullUriPipe } from './to-full-uri.pipe';
 import { EnumPipe } from './enum-pipe';
+import { CapitalizeFirstLetterPipe } from './capitalize-first-letter-pipe';
 
 @NgModule({
   imports: [],
@@ -10,8 +11,9 @@ import { EnumPipe } from './enum-pipe';
     LabelPipe,
     ReversePipe,
     ToFullUriPipe,
-    EnumPipe
+    EnumPipe,
+    CapitalizeFirstLetterPipe
   ],
-  exports: [LabelPipe, ReversePipe, ToFullUriPipe, EnumPipe],
+  exports: [LabelPipe, ReversePipe, ToFullUriPipe, EnumPipe, CapitalizeFirstLetterPipe],
 })
 export class PipesModule {}
