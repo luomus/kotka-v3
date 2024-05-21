4bfcc812cffc5dae4250a1b9c10acc65723511ad
aorin <meeri.rannisto@gmail.com>
2024-05-21 16:55:04 +0300

    transaction pdf sheet material section fine-tuning

--- apps/kotka/src/app/+transactions/transaction-pdf-sheets/sheet-components/transaction-sheet-material/transaction-sheet-material.ts
+++ apps/kotka/src/app/+transactions/transaction-pdf-sheets/sheet-components/transaction-sheet-material/transaction-sheet-material.ts
@@ -1,8 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { SpecimenTransaction } from '@luomus/laji-schema';
 import { CommonModule } from '@angular/common';
 import { PipesModule } from '@kotka/pipes';
 import { TransactionTypeLabelPipe } from '../../pipes/transaction-type-label.pipe';
+
+type SpecimenIdField = keyof Pick<SpecimenTransaction, 'awayIDs' | 'returnedIDs' | 'missingIDs'>;
+type SpecimenCountField = keyof Pick<SpecimenTransaction, 'awayCount' | 'returnedCount' | 'missingCount'>
 
 @Component({
   standalone: true,
@@ -10,10 +13,44 @@ import { TransactionTypeLabelPipe } from '../../pipes/transaction-type-label.pip
   selector: 'kotka-transaction-sheet-material',
   templateUrl: './transaction-sheet-material.html'
 })
-export class TransactionSheetMaterialComponent {
+export class TransactionSheetMaterialComponent implements OnChanges {
   @Input({ required: true }) data!: SpecimenTransaction;
 
-  get totalAwayCount(): number {
-    return (this.data.awayIDs || []).length + (this.data.awayCount || 0);
+  originalTotalCount = 0;
+  originalCountWithIds = 0;
+  originalCountWithoutIds = 0;
+
+  awayTotalCount = 0;
+
+  allSpecimens: string[] = [];
+
+  ngOnChanges() {
+    this.originalTotalCount = 0;
+    this.originalCountWithIds = 0;
+    this.originalCountWithoutIds = 0;
+    this.awayTotalCount = 0;
+    this.allSpecimens = [];
+
+    const specimenIdFields: SpecimenIdField[] = ['awayIDs', 'returnedIDs', 'missingIDs'];
+    const specimenCountFields: SpecimenCountField[] = ['awayCount', 'returnedCount', 'missingCount'];
+
+    specimenIdFields.forEach(field => {
+      const value = (this.data[field] || []).length;
+      this.originalCountWithIds += value;
+      this.originalTotalCount += value;
+      if (field === 'awayIDs') {
+        this.awayTotalCount += value;
+      }
+      this.allSpecimens = this.allSpecimens.concat(this.data[field] || []);
+    });
+
+    specimenCountFields.forEach(field => {
+      const value = this.data[field] || 0;
+      this.originalCountWithoutIds += value;
+      this.originalTotalCount += value;
+      if (field === 'awayCount') {
+        this.awayTotalCount += value;
+      }
+    });
   }
 }
