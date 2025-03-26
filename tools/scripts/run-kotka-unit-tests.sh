fc686aa268b7376ee6740c3e9efb8187d7097318
aorin <meeri.rannisto@gmail.com>
2025-03-26 15:23:03 +0200

    unit test script removes docker volumes after and before running the tests

--- tools/scripts/run-kotka-unit-tests.sh
+++ tools/scripts/run-kotka-unit-tests.sh
@@ -4,9 +4,13 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPT_PATH}/../../
 
+docker-compose --env-file .env.e2e down --remove-orphans -v
+
 exit_code=0
 docker-compose --env-file .env.e2e run --rm --no-deps --entrypoint="npm run test:all" kotka-api \
 || exit_code=$?
+
+docker-compose --env-file .env.e2e down --remove-orphans -v
 
 if [[ $exit_code -eq 0 ]]; then
     echo "Tests passed"
