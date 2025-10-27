8747696e94e1884d2b13114d464ddfc1ae53c87f
aorin <meeri.rannisto@gmail.com>
2025-10-27 11:28:59 +0200

    fix unit test script failing on ci

--- tools/scripts/run-kotka-unit-tests.sh
+++ tools/scripts/run-kotka-unit-tests.sh
@@ -4,13 +4,13 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPT_PATH}/../../
 
-docker-compose --env-file .env.e2e down --remove-orphans -v
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e down --remove-orphans -v
 
 exit_code=0
-docker-compose --env-file .env.e2e run --rm --no-deps --entrypoint="npm run test:all" kotka-api \
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e run --rm --no-deps --entrypoint="npm run test:all" kotka-api \
 || exit_code=$?
 
-docker-compose --env-file .env.e2e down --remove-orphans -v
+docker-compose -f ./docker-compose.e2e.yml --env-file .env.e2e down --remove-orphans -v
 
 if [[ $exit_code -eq 0 ]]; then
     echo "Tests passed"
