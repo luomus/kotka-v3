878e2e392be1a3cc9373b04175b9edb167cd37a4
aorin <meeri.rannisto@gmail.com>
2025-10-27 12:41:53 +0200

    fix lint error

--- apps/kotka-api/src/app/laji-api/laji-api.controller.ts
+++ apps/kotka-api/src/app/laji-api/laji-api.controller.ts
@@ -102,7 +102,7 @@ export class LajiApiController {
     }
   }
 
-  private pathRewrite(path: string, req: UserRequest): string {
+  private pathRewrite(path: string): string {
     return path.replace(this.lajiApiBase, '');
   }
 
