fb61e00e0f45fc4f63745e0fed0d32cbc0aef197
Markus <markusjuhani.lumme@gmail.com>
2026-03-10 10:56:47 +0200

    Forgotten utlis for validation from one of the last commits.

--- libs/shared/utils/src/lib/basic-utils.ts
+++ libs/shared/utils/src/lib/basic-utils.ts
@@ -1,5 +1,29 @@
 import { JSONPath } from 'jsonpath-plus';
 
+export interface JSONPathAllResponse {
+  value: any;
+  path: string;
+  pointer: string;
+  parent: any;
+  parentProperty: string;
+}
+
 export function parseJSONPointer(data: object, path: string) {
   return JSONPath({ path: path.split('/'), json: data || {}, wrap: false });
+}
+
+export function parseStoreSearchPath(path: string) {
+  let storeSearchPath = '';
+
+  if (path.startsWith('/')) path = path.slice(1);
+
+  const splitPath = path.split('/');
+
+  splitPath.forEach((part, idx) => {
+    if(!/^\d+$/.test(part)) {
+      storeSearchPath += (idx === 0 ? part : '.' + part);
+    }
+  });
+
+  return storeSearchPath;
 }
