7128a9135664344c1ef1c53136936d0b54cbf821
aorin <meeri.rannisto@gmail.com>
2026-04-28 13:34:17 +0300

    validation interceptor returns errors in the correct format

--- apps/kotka-api/src/app/shared/interceptors/validator.interceptor.ts
+++ apps/kotka-api/src/app/shared/interceptors/validator.interceptor.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ValidationService } from '@kotka/api/services';
-import { KotkaDocumentObjectFullType } from '@kotka/shared/models';
+import { ApiValidationError, KotkaDocumentObjectFullType } from '@kotka/shared/models';
 
 @Injectable()
 export class ValidatorInterceptor implements NestInterceptor {
@@ -31,37 +31,18 @@ export class ValidatorInterceptor implements NestInterceptor {
     const errors = await this.validationService.validate(req.body, type);
 
     if (errors) {
-      throw new UnprocessableEntityException('Unprocessable Entity', this.formatErrorDetails(errors, undefined));
+      const errorResponse: ApiValidationError = {
+        errorCode: 'VALIDATION_EXCEPTION',
+        details: Object.keys(errors).reduce((result, key) => {
+          const newKey = key.replace(/\[(\d+)]/g, '/$1').replace(/\./g, '/');
+          result[newKey] = errors[key];
+          return result;
+        }, {}),
+      };
+
+      throw new UnprocessableEntityException(errorResponse);
     }
 
     return next.handle();
-  }
-
-  formatErrorDetails(errors, targetType) {
-    switch (targetType) {
-      case 'jsonPath':
-        return errors;
-      default:
-        return this.errorsToObj(errors);
-    }
-  }
-
-  errorsToObj(errors) {
-    const result = {};
-    Object.keys(errors).map(path => {
-      const re = /[.[\]]/;
-      const parts = path.split(re).filter(value => value !== '');
-      let pointer = result;
-      let now = parts.shift();
-      while (parts.length > 0) {
-        if (!pointer[now]) {
-          pointer[now] = {};
-        }
-        pointer = pointer[now];
-        now = parts.shift();
-      }
-      pointer[now] = {errors: errors[path]};
-    });
-    return result;
   }
 }
