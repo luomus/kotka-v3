39bd0748663c549a64da9eae049a4314e29940a3
Markus <markusjuhani.lumme@gmail.com>
2026-08-14 16:02:39 +0300

    Removed imports of uncommit meodules.

--- apps/kotka-cli/src/kotka-cli.module.ts
+++ apps/kotka-cli/src/kotka-cli.module.ts
@@ -3,16 +3,10 @@ https://docs.nestjs.com/modules
 */
 
 import { ApiServicesModule } from '@kotka/api/services';
-import { SpecimenSearchModule } from '@kotka/api/specimen-search';
 import { Module } from '@nestjs/common';
 import { ConsoleModule } from 'nestjs-console';
 import { MigrateCommand } from './commands/migrate.command';
 import { SequenceCommand } from './commands/sequence.command';
-import { MigrateTestDocumentsCommand } from './commands/migrate-test-documents.command';
-import { TaxonLinkingModule } from '@kotka/api/taxon-linking';
-import { ElasticModule } from '@kotka/api/elasticsearch';
-import { TaxonUpdateCommand } from './commands/taxon-update.command';
-import { TestingCommand } from './commands/testing.command';
 import { CacheModule } from '@kotka/api/cache';
 
 @Module({
@@ -21,11 +15,8 @@ import { CacheModule } from '@kotka/api/cache';
     CacheModule,
     ConsoleModule,
     ApiServicesModule,
-    TaxonLinkingModule,
-    ElasticModule,
-    SpecimenSearchModule
   ],
-  controllers: [MigrateCommand, SequenceCommand, MigrateTestDocumentsCommand, TaxonUpdateCommand, TestingCommand ],
-  providers: [MigrateCommand, SequenceCommand, MigrateTestDocumentsCommand, TaxonUpdateCommand, TestingCommand ]
+  controllers: [MigrateCommand, SequenceCommand ],
+  providers: [MigrateCommand, SequenceCommand ]
 })
 export class KotkaCliModule {}
